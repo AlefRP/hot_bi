251a4a8efbbdf61f847b1265758a61bf325ba109
alefrp <alefrodrigopereira@yahoo.com.br>
2024-07-08 17:06:56 -0300

    feat: bi hot

--- docker/init/mysql/init_dw.sql
+++ docker/init/mysql/init_dw.sql
@@ -1,46 +1,35 @@
--- Tabela Dimensão: dim_course
-CREATE TABLE IF NOT EXISTS dim_course (
-    course_key INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    course_title VARCHAR(255) NOT NULL,
-    url TEXT NOT NULL,
-    is_paid BOOLEAN NOT NULL
-);
-
--- Tabela Dimensão: dim_time
-CREATE TABLE IF NOT EXISTS dim_time (
-    time_key INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    published_timestamp TIMESTAMP,
-    year INT,
-    month INT,
-    day INT
-);
-
--- Tabela Dimensão: dim_level
+-- dim_level
 CREATE TABLE IF NOT EXISTS dim_level (
     level_key INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     level VARCHAR(50) NOT NULL
 );
 
--- Tabela Dimensão: dim_subject
+-- dim_subject
 CREATE TABLE IF NOT EXISTS dim_subject (
     subject_key INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     subject VARCHAR(255) NOT NULL
 );
 
+-- dim_paid
+CREATE TABLE IF NOT EXISTS dim_paid (
+    paid_key INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    is_paid VARCHAR(10) NOT NULL
+);
+
 -- Tabela Fato: fact_courses
 CREATE TABLE IF NOT EXISTS fact_courses (
     course_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    course_key INT UNSIGNED,
-    time_key INT UNSIGNED,
+    published_date TIMESTAMP NOT NULL,
+    course_title VARCHAR(255) NOT NULL,
     level_key INT UNSIGNED,
     subject_key INT UNSIGNED,
-    price VARCHAR(50),
+    paid_key INT UNSIGNED,
+    price FLOAT,
     num_subscribers INT,
     num_reviews INT,
     num_lectures INT,
     content_duration FLOAT,
-    FOREIGN KEY (course_key) REFERENCES dim_course(course_key),
-    FOREIGN KEY (time_key) REFERENCES dim_time(time_key),
     FOREIGN KEY (level_key) REFERENCES dim_level(level_key),
-    FOREIGN KEY (subject_key) REFERENCES dim_subject(subject_key)
+    FOREIGN KEY (subject_key) REFERENCES dim_subject(subject_key),
+    FOREIGN KEY (paid_key) REFERENCES dim_paid(paid_key)
 );
